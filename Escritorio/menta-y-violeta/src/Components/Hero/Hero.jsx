c99a0c4fc117c78edaa3a50de3956109bccb74ea
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-03-07 15:08:12 -0500

    progreso en hero

--- Escritorio/menta-y-violeta/src/Components/Hero/Hero.jsx
+++ Escritorio/menta-y-violeta/src/Components/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import './Hero.css';
 import React from 'react';
-import HeroImgage from '../../Resources/Images/makeup.png';
+import HeroImage from '../../Resources/Images/makeup.png';
 
 const Hero = () => {
   return (
     <div className="container__hero">
       <section className="hero">
         <h1 className="hero__h1"> Bienvenidos<br/> a Menta y<br/> Violeta</h1> 
+        <img src={HeroImage} alt="Hero image" className="header__img"/>
       </section>
     </div>
   )
